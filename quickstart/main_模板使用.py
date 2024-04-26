ae7b7ac06dcea0fc7760f5e4540a07ba5ac0cd1b
HopinNotDisaster <syq3315813594@gmail.com>
2024-04-26 17:02:06 +0800

    添加一个"大的项目"blog，完成！

--- quickstart/main_模板使用.py
+++ quickstart/main_模板使用.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", name="qiku", age=10)
+    return render_template("base.html", name="qiku", age=10)
 
 
 def main():
